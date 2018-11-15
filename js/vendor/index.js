2b75de9559c5d9816dbe6962fd803523c25a0816
anhquoc4062 <anhquoc4062@gmail.com>
2018-11-15 21:43:01 +0700

    abc

---
+++ js/vendor/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(window).scroll(function () { 
+        var currentPosition = $(document).scrollTop();
+        console.log(currentPosition);
+        if(currentPosition >= 1400){
+            $('#coffee').fadeTo(2000, 1).animate({'margin-top': '30px'}, {duration: '3000', queue: false}, function() {
+                // Animation complete.
+            });
+        }
+    });
+    
+});
